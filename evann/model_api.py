e539e80737945be453d1077f16a0d96cba528ac0
ArnoutDevos <arnoutdev@gmail.com>
2019-11-16 16:44:34 +0100

    TensorFlow model is initialized in class, 'api' function only calls inference

--- evann/model_api.py
+++ evann/model_api.py
@@ -6,12 +6,20 @@ import numpy as np
 # model = tensorflow.keras.models.load_model('./models/keras_model_cookies.h5')
 class ModelApi(object):
     def __init__(self):
+        self.session = tensorflow.Session()
+        tensorflow.keras.backend.set_session(self.session)
+        init = tensorflow.global_variables_initializer()
+        self.session.run(init)
+
         self.model = tensorflow.keras.models.load_model(
             './models/keras_model_cookies.h5')
+        self.model._make_predict_function()
 
     def get_class_id(self, path):
         # Load the model
         image = Image.open(path, "r")
+
+        print("opened image")
         # image = Image.open('./training_data/blue_cookie.png')
 
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -28,6 +36,12 @@ class ModelApi(object):
         data[0] = normalized_image_array
 
         # run the inference
-        prediction = self.model.predict(data)
-        max_pred = np.argmax(prediction)
+        print("before inference")
+
+        with self.session.as_default():
+            with self.session.graph.as_default():
+                prediction = self.model.predict(data)
+                max_pred = np.argmax(prediction)
+
+        print(prediction)
         return dic[max_pred]
